fe06b0b887f646aa2898d82b8b16464acfca761f
AdamMutzell <admt21@student.bth.se>
2024-11-06 14:50:14 +0000

    fix

--- adversarial/adversarial/server_app.py
+++ adversarial/adversarial/server_app.py
@@ -5,33 +5,6 @@ from adversarial.task import Net, get_weights
 import pandas as pd
 
 counter = 0
-def weighted_average(metrics: list[tuple[int, dict[str, float]]]):
-    global counter
-
-    # Multiply accuracy of each client by number of examples used
-    accuracies = [num_examples*m["accuracy"] for num_examples, m in metrics]
-    kappa = [num_examples*m["kappa"] for num_examples, m in metrics]
-    f1 = [num_examples*m["f1"] for num_examples, m in metrics]
-    roc_auc = [num_examples*m["roc_auc"] for num_examples, m in metrics]
-    examples = [num_examples for num_examples, _ in metrics]
-    print(metrics)
-    n_examples = sum(examples)
-    main_avg = {"accuracy": sum(accuracies) / n_examples,
-                "kappa": sum(kappa) / n_examples,
-                "f1": sum(f1) / n_examples,
-                "roc_auc": sum(roc_auc) / n_examples}
-    log = pd.DataFrame(main_avg, index=[0])
-    log.loc[0] = [value for value in list(main_avg.values())]
-    for i in range(len(metrics)):
-        log.loc[i+1] = {key: value for key, value in metrics[i][1].items()}
-
-    counter += 1
-    log.to_csv("adversarial/logging/log" + str(counter) + ".csv")
-    # Aggregate and return custom metric (weighted average)
-    return {"accuracy": sum(accuracies) / sum(examples) , 
-            "accuracy_per_client": [m["accuracy"] for _, m in metrics],
-            "kappa": sum(kappa) / sum(examples), 
-            "kappa_per_client": [m["kappa"] for _, m in metrics]}
 from adversarial.server_aid import weighted_average, CustomClientConfigStrategy, fit_config
 
 def server_fn(context: Context):
