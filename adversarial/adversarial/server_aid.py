6cc355f5c38fde92665f757f547188512098eee2
Uplink036 <oliversjodin02@gmail.com>
2024-11-08 13:48:14 +0000

    Add imports for fit average

--- adversarial/adversarial/server_aid.py
+++ adversarial/adversarial/server_aid.py
@@ -1,6 +1,11 @@
-from flwr.common import FitIns, Parameters
+
+from functools import partial, reduce
+from typing import Optional, Union
+from flwr.common import FitIns, Parameters, FitRes, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, NDArrays
+from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy import FedAvg, FedProx
 import pandas as pd
+import numpy as np
 from adversarial.configs import Configs
 import os
 import dotenv
